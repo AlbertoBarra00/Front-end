5a696c118ca929b08eb9cd10ef8c10eb1863b20d
Alberto <bagheebn@gmail.com>
2022-04-11 00:16:53 +0200

    front

---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PersonaComponent } from './views/persona/persona.component';
+import { HttpClientModule } from '@angular/common/http';
+import {MatTableModule} from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { ConfirmDialogComponent } from './views/persona/confirm-dialog/confirm-dialog.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { PersonaModalComponent } from './views/persona/persona-modal/persona-modal.component';
+import { FormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatPaginatorIntl, MatPaginatorModule} from '@angular/material/paginator'
+import { MatSortModule } from '@angular/material/sort';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PersonaComponent,
+    ConfirmDialogComponent,
+    PersonaModalComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatTableModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatSortModule
+  ],
+  providers: [
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
